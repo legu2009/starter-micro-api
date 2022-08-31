880136242e80de6b45506d860a7e8508270922bd
legu2009 <89415119@qq.com>
2022-08-31 13:50:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-var http = require('http');
+const http = require("http"),
+    https = require("https");
+
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+    const origin = req.headers["proxy_wgu_origin"];
+    if (!origin) {
+        res.writeHead("200", {
+            "Content-Type": "text/plain",
+        });
+        res.end("hello");
+        return;
+    }
+
+    const newUrl = new URL(origin);
+    delete req.headers["proxy_wgu_origin"];
+    req.headers.host = newUrl.host;
+    const proxyReq = (newUrl.protocol === "https" ? https : http).request(newUrl.toString(), {
+        method: req.method,
+        headers: req.headers,
+    }, proxyRes => {
+        res.writeHead(proxyRes.statusCode, proxyRes.headers);
+        proxyRes.pipe(res);
+    });
+    req.pipe(proxyReq);
+    proxyReq.on("error", error => {
+        res.end();
+    });
+}).listen(process.env.port || 3000);
+
+
